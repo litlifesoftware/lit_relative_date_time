28cbba61c88982bbe9c50a41d76baff48f4dbed7
eightgran <eightgran@gmail.com>
2021-09-03 15:52:10 +0200

    Reorganize time constants

---
+++ lib/data/time_constants.dart
@@ -0,0 +1,17 @@
+/// The miliseconds per second `1000`.
+const double msPerSecond = 1000.0;
+
+/// The milliseconds per minute `60000`.
+const double msPerMinute = 60000.0;
+
+/// The milliseconds per hour `3.6e+6`.
+const double msPerHour = 3.6e+6;
+
+/// The miliseconds per day `8.64e+7`.
+const double msPerDay = 8.64e+7;
+
+/// The miliseconds per month `2.628e+9`.
+const double msPerMonth = 2.628e+9;
+
+/// The miliseconds per year `3.154e+10`.
+const double msPerYear = 3.154e+10;
