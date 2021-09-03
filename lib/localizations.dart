159809ec7ce4f36006363306345d23af78945d23
eightgran <eightgran@gmail.com>
2021-09-03 15:51:15 +0200

    Create `localizations` library

---
+++ lib/localizations.dart
@@ -0,0 +1,7 @@
+library localizations;
+
+export 'localization/de.dart';
+export 'localization/en.dart';
+export 'localization/ru.dart';
+export 'localization/default.dart';
+export 'localization/all.dart';
