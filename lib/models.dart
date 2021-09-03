2d8bcdc8a372d3a9114e9581d4d4865b4281740f
eightgran <eightgran@gmail.com>
2021-09-03 15:51:29 +0200

    Create `models` library

---
+++ lib/models.dart
@@ -0,0 +1,6 @@
+/// A list of model classes used to store and mutate data across this package.
+library models;
+
+export 'model/lit_time_unit.dart';
+export 'model/relative_date_time.dart';
+export 'model/time_difference.dart';
