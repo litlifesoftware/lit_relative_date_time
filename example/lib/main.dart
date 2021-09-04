6f96b9cbc65ea28a690d4cdfc16918f4f224173d
eightgran <eightgran@gmail.com>
2021-09-04 19:43:37 +0200

    Refactor `example` app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Lit Relative Date Time Demo',
+      title: 'LitRelativeDateTime',
       // Set the localization delegates you want to use.
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
@@ -36,94 +36,58 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  AnimationController _animationController;
-
-  DateTime _lastPressed;
-
-  /// Get a relative date using the current [DateTime] and the [DateTime] the
-  /// button has been pressed the last.
-  RelativeDateTime get relativeDateTime {
-    RelativeDateTime _relativeDateTime =
-        RelativeDateTime(dateTime: DateTime.now(), other: _lastPressed);
-    return _relativeDateTime;
-  }
-
-  /// Get a formatted and localized date time in human-readable format.
-  RelativeDateFormat get relativeDateFormatter {
-    RelativeDateFormat _relativeDateFormatter = RelativeDateFormat(
-      Localizations.localeOf(context),
-    );
-    return _relativeDateFormatter;
-  }
+  DateTime? _lastPressed;
 
   /// Reset the [_lastPressed] date time and start the animation.
   void _onPressed() {
     setState(() {
       _lastPressed = DateTime.now();
     });
-    if (!_animationController.isAnimating) {
-      _animationController.repeat(reverse: true);
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _animationController = AnimationController(
-      vsync: this,
-      duration: Duration(
-        milliseconds: 1000,
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Lit Relative Date Time Demo"),
+        backgroundColor: Colors.black,
+        title: Text("LitRelativeDateTime"),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _onPressed,
         child: Icon(_lastPressed != null ? Icons.restore : Icons.play_arrow),
       ),
-      body: AnimatedBuilder(
-        animation: _animationController,
-        builder: (context, _) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    "Last pressed:",
-                    textAlign: TextAlign.end,
-                  ),
-                ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                "Last pressed:",
+                textAlign: TextAlign.end,
               ),
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    _lastPressed != null
-                        ? relativeDateFormatter.format(relativeDateTime)
-                        : "Not Pressed Yet",
-                    textAlign: TextAlign.end,
-                  ),
-                ),
-              ),
-            ],
-          );
-        },
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: _lastPressed != null
+                  ? AnimatedRelativeDateTimeBuilder(
+                      date: _lastPressed!,
+                      builder: (relDateTime, formatted) {
+                        return Text(
+                          formatted,
+                        );
+                      },
+                    )
+                  : Text(
+                      "Not Pressed Yet",
+                    ),
+            ),
+          ),
+        ],
       ),
     );
   }
