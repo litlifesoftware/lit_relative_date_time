94e05d6f186b76ca1215f4b9a08541adb908d50e
eightgran <eightgran@gmail.com>
2021-11-17 21:07:52 +0100

    Fix localization on negative time differences - Use absolute value for singular check - Document TimeDifferenceController

--- lib/controller/time_difference_controller.dart
+++ lib/controller/time_difference_controller.dart
@@ -1,25 +1,26 @@
 import 'package:lit_relative_date_time/lit_relative_date_time.dart';
 
+/// A LitRelativeDateTime `controller` class implementing checks performed on
+/// [TimeDifference] model classes.
 class TimeDifferenceController {
+  /// The time difference.
   final TimeDifference timeDifference;
 
+  /// Creates a [TimeDifferenceController].
   const TimeDifferenceController(this.timeDifference);
 
   /// States whether the difference unit is `second`.
-
   bool get isSmallestUnit {
     return timeDifference.unit == LitTimeUnit.second;
   }
 
   /// States whether the difference value is `0`.
-
   bool get isZero {
-    return timeDifference.value == 0;
+    return timeDifference.value.abs() == 0;
   }
 
   /// States whether the difference value is `1`.
-
   bool get isSingular {
-    return timeDifference.value == 1;
+    return timeDifference.value.abs() == 1;
   }
 }
